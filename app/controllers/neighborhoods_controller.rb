56d4bb08e7c11d4b8456a7b39a83cb4633f706c6
Allison Larson <allisonlarson91@gmail.com>
2014-10-01 20:45:26 -0600

    map fucking shows up

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -3,6 +3,7 @@ class NeighborhoodsController < ApplicationController
   end
 
   def show
+    @filters = ['Restaurants', 'Bars', 'Ice Cream', 'Coffee', 'Parks']
     @neighborhood = Neighborhood.find_by(params[:search])
   end
 end
