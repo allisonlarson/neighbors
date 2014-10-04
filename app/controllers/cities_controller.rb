848c706fb9cb6cc9d88379777a5c2af12ba24919
Allison Larson <allisonlarson91@gmail.com>
2014-10-04 12:53:13 -0600

    change variable names fixes #7

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,6 +2,5 @@ class CitiesController < ApplicationController
   def show
     @filters = ['Restaurants', 'Bars', 'Ice Cream', 'Coffee', 'Parks']
     @area = City.find(params[:id])
-    @neighborhood_area = @area.neighborhoods.map {|n| n.coordinates }
   end
 end
