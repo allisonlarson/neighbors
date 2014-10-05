33c2897c91d823286db5b1d26c887b40e5b18f2c
Allison Larson <allisonlarson91@gmail.com>
2014-10-04 18:16:39 -0600

    denver map is clickable

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,7 @@
 class CitiesController < ApplicationController
   def show
     @area = City.friendly.find(params[:id])
-    @filters = @area.neighborhoods
+    @filters = @area.neighborhoods.sort_by {|n| n.name}
     @neighborhoods = @filters.map {|n| n.coordinates}
   end
 end
