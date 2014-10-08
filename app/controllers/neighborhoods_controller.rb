21e991d563ad58ed0092c2ab75c5bd7f65d1138f
Allison Larson <allisonlarson91@gmail.com>
2014-10-08 14:50:16 -0600

    move excess api shit out of controllers

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,30 +1,16 @@
+require_relative '../models/foursquare'
+
 class NeighborhoodsController < ApplicationController
   require 'json'
+
   def index
   end
 
   def show
     @area = Neighborhood.friendly.find(params[:id])
     @filters = ['Restaurant', 'Bars', 'Ice Cream', 'Parks']
-    @establishments = establish_establishments
+    foursquare = Foursquare.new(@area)
+    @establishments = foursquare.establish_establishments
   end
 
-  def connect
-    Faraday.new(:url => 'https://api.foursquare.com') do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
-  end
-
-  def get_establishments
-    lat = @area.coordinates.first.lat
-    lon = @area.coordinates.first.lon
-    connect.get "/v2/venues/explore?client_id=4YV00VWDCJRCFRN4YMYLYNPHTMIR2YHAYW22KATQGACL5AEF&client_secret=I4QOZRRED0QQ0TNPEYQ3I2AUSNEL2AE4GJDYF10RORCTURVE&ll=#{lat},#{lon}&v=20130815&m=swarm"
-  end
-
-  def establish_establishments
-    venue = JSON.load(get_establishments.body)
-    venue.map {|v| venue['response']['groups'].first['items'].map do |i| i['venue']end}.flatten
-  end
 end
---
+++ app/models/foursquare.rb
@@ -0,0 +1,45 @@
+class Foursquare
+
+  BASE_URL = 'https://api.foursquare.com/v2/'
+
+  def initialize(neighborhood)
+    @neighborhood = neighborhood
+  end
+
+  def venue_search_url
+    BASE_URL + 'venues/explore?' + {
+      client_id: '4YV00VWDCJRCFRN4YMYLYNPHTMIR2YHAYW22KATQGACL5AEF',
+      client_secret: 'I4QOZRRED0QQ0TNPEYQ3I2AUSNEL2AE4GJDYF10RORCTURVE',
+      limit: 50,
+      ll: lat_lon,
+      v: '20130118',
+      m: 'swarm',
+      openNow: true,
+    }.to_query
+  end
+
+  def establish_establishments
+    venue = JSON.load(get_establishments.body)
+    venue.map {|v| venue['response']['groups'].first['items'].map do |i| i['venue']end}.flatten
+  end
+
+  private
+
+  def lat_lon
+    coord = @neighborhood.coordinates.sample
+    "#{coord.lat},#{coord.lon}"
+  end
+
+  def connect
+    Faraday.new(:url => BASE_URL) do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+  end
+
+  def get_establishments
+    connect.get venue_search_url
+  end
+
+end
