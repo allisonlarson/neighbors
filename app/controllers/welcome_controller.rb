c23357fefb709333d54337dc0fb52024e958acc4
Allison Larson <allisonlarson91@gmail.com>
2014-10-16 10:00:31 -0600

    add rescue

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,19 @@
 class WelcomeController < ApplicationController
 	def search
-    result = PgSearch.multisearch(params[:search]).first
-      if result.searchable_type == "Neighborhood"
-				neighborhood_redirect(result)
-      elsif result.searchable_type == "City"
-				city_redirect(result)
-      end
+		begin
+	    result = PgSearch.multisearch(params[:search]).first
+	      if result.searchable_type == "Neighborhood"
+					neighborhood_redirect(result)
+	      elsif result.searchable_type == "City"
+					city_redirect(result)
+	      end
+		rescue
+			redirect_to root_path
+		end
   end
 
 	private
-	
+
 	def city_redirect(result)
 		c = City.find(result.searchable_id)
 		redirect_to city_path(c)
